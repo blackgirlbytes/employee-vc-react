820e7104ca8e217bbbbc893471c9cd2244be3158
Rizel Scarlett <rizel@tbd.email>
2024-01-02 12:35:11 -0500

    help

--- src/components/Employee.jsx
+++ src/components/Employee.jsx
@@ -2,23 +2,8 @@ import { useState, useEffect } from 'react';
 // import { DidDhtMethod } from '@web5/dids';
 
 export default function Employee() {
-    const [employeeDid, setEmployeeDid] = useState('');
-    const [vcJwt, setVcJwt] = useState('');
 
-    // useEffect(() => {
-    //     const createDid = async () => {
-    //         const didObject = await DidDhtMethod.create();
-    //         setEmployeeDid(didObject.did);
-    //     };
-
-    //     createDid();
-    // }, []);
-
-    // const copyToClipboard = () => {
-    //     navigator.clipboard.writeText(employeeDid);
-    //     alert('DID copied to clipboard!');
-    // };
-   setEmployeeDid("did:web:u0x1b.5g6.2g3.4g5");
+   const employeeDid = "did:web:u0x1b.5g6.2g3.4g5"
     return (
         <div>
             <h1>Employee</h1>
