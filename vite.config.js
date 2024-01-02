505e960caae8cc5d1b559f17771eff4f34d0bc77
Rizel Scarlett <rizel@tbd.email>
2024-01-02 13:09:06 -0500

    parens

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import commonjs from '@rollup/plugin-commonjs';
-import nodePolyfills from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // ... other imports ...
 
